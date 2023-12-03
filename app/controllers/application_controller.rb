99d0221abc251ab98abef6d836d26bbbbd833216
Dylan <dylanquach@gmail.com>
2023-12-02 16:50:30 -0800

    Add rails routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,37 @@
 class ApplicationController < ActionController::API
+    include ActionController::RequestForgeryProtection
+
+    protect_from_forgery with: :exception
+    before_action :attach_authenticity_token, :snake_case_params
+
+    def current_user
+      @current_user ||= User.find_by(session_token: session[:session_token])
+    end
+
+    def login!(user)
+      session[:session_token] = user.reset_session_token!
+      @current_user = user
+    end
+
+    def logout!
+      current_user.reset_session_token! if current_user
+      session[:session_token] = nil
+      @current_user = nil
+    end
+
+    def require_logged_in
+      unless current_user
+        render json: { message: 'Unauthorized' }, status: :unauthorized
+      end
+    end
+
+    private
+
+    def snake_case_params
+        params.deep_transform_keys!(&:underscore)
+    end
+
+    def attach_authenticity_token
+      headers['X-CSRF-Token'] = masked_authenticity_token(session)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  namespace :api, defaults: { format: :json } do
+    resources :users, only: [:show, :create, :update]
+    resource :session, only: [:show, :create, :destroy]
+  end
 end
