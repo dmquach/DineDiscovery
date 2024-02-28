889c3244ff14ddce15f80d9575d2eb29f1cf47ed
Dylan <dylanquach@gmail.com>
2024-02-27 23:13:04 -0800

    Modify errors

---
+++ frontend/src/store/error.js
@@ -0,0 +1,32 @@
+const RECEIVE_ERROR = "errors/sendError";
+
+export const receiveError = (errors) => {
+  return {
+    type: RECEIVE_ERROR,
+    errors,
+  };
+};
+
+
+export const sendError =(errors)=>async(dispatch)=>{
+    const res = await fetch('/api/errors',{
+        method:"POST",
+        headers:{
+            "Content-Type":"application/json"
+        },
+        body:JSON.stringify(errors)
+
+    })
+}
+
+const errorReducer = (state = {}, action) => {
+  switch (action.type) {
+    case RECEIVE_ERROR: {
+      return { ...state, errors: action.errors };
+    }
+    default:
+      return state;
+  }
+};
+
+export default errorReducer
