8f82f712ae665dc3d55a9e8111efc5ef1ab753f2
Dylan <dylanquach@gmail.com>
2024-02-29 01:08:18 -0800

    Add star rating

---
+++ frontend/src/components/Stars/StarRating.jsx
@@ -0,0 +1,20 @@
+import "./starRating.css";
+
+const FixedStarRating = ({ rating,styles }) => {
+
+  return (
+
+    <div className="star-rating" style={styles}>
+      {[...Array(5)].map((star, i) => {
+        i += 1;
+        return (
+          <span key={i + 99} className={i <= rating ? "on" :"off"}>
+            <i className="fa-solid fa-star"></i>
+          </span>
+        );
+      })}
+    </div>
+  );
+};
+
+export default FixedStarRating;
