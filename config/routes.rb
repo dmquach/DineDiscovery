5d144d7fd496da6d60ebb724e3eff445bafa2f1a
Dylan <dylanquach@gmail.com>
2024-03-12 09:11:09 -0700

    Fix routing for reviews

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,25 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # post "api/test", to: "application#test"
+
   namespace :api, defaults: { format: :json } do
-    resources :users, only: [:show, :create, :update]
+    resources :posts, only: [:show, :index, :create]
+    resources :users, only: [:create, :index, :show]
     resource :session, only: [:show, :create, :destroy]
+    resources :businesses, only: [:index, :create, :show, :update, :destroy] do
+      get :unreviewed, on: :collection
+      get :search, on: :collection
+    end
+    resources :reviews, only: [:index, :create, :show, :update, :destroy] do
+      get :latest, on: :collection
+      patch :delete_image, on: :member
+    end
   end
+
+  get "*path",
+    to: "static_pages#frontend",
+    constraints: lambda { |req| !req.xhr? && req.format.html? }
+  root 'static_pages#frontend',
+    constraints: lambda { |req| !req.xhr? && req.format.html? }
 end
