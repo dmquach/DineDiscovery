0acea1dee06b73a7d824e7767c15d062c9e7d19b
Dylan <dylanquach@gmail.com>
2024-03-22 23:32:51 -0700

    Google maps api testing file

---
+++ frontend/src/components/GoogleMaps/GoogleMapsAPI.jsx
@@ -0,0 +1,11 @@
+
+import "./map.css";
+import { useState, useEffect } from "react";
+import {
+  GoogleMap,
+  useLoadScript,
+  Marker,
+  InfoWindow,
+  OverlayView,
+} from "@react-google-maps/api";
+import { Link } from "react-router-dom";
