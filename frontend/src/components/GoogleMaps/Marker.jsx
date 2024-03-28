d92ef63cc6035a05c1b7d9406e8b39ad568dcb30
Dylan <dylanquach@gmail.com>
2024-03-27 23:34:35 -0700

    Add business to map

--- frontend/src/components/GoogleMaps/Marker.jsx
+++ frontend/src/components/GoogleMaps/Marker.jsx
@@ -11,4 +11,24 @@ export default function BizGMap({ business, class_name }) {
     googleMapsApiKey: process.env.REACT_APP_MAPS_API_KEY,
     libraries: place,
   });
+
+  if (!business) {
+    return null;
+  }
+  if (!isLoaded) return <div>Loading...</div>;
+  return <Map business={business} class_name={class_name} />;
+}
+
+function Map({ business }) {
+  // const center = useMemo(
+  //   () => ({ lat: business.lat, lng: business.lng }),
+  //   [business]
+  // );
+  // const [selected, setSelected] = useState(null);
+
+  return (
+    <>
+
+    </>
+  );
 }
