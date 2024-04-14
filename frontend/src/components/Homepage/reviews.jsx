90cdaa0aa5a63c2fbde6bedd53cb7406b170ece8
Dylan <dylanquach@gmail.com>
2024-04-13 23:42:02 -0700

    Create review card singles

---
+++ frontend/src/components/Homepage/reviews.jsx
@@ -0,0 +1,5 @@
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchLatestReviews } from "../../store/review";
+
+import reviewCard from "./reviewCard";
