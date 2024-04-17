c815f79b55afe8d785c0db49cd92e4844d75eef8
Dylan <dylanquach@gmail.com>
2024-04-16 21:14:51 -0700

    Add latest reviews

--- frontend/src/components/Homepage/reviews.jsx
+++ frontend/src/components/Homepage/reviews.jsx
@@ -7,10 +7,16 @@ import "./reviewCard.css";
 
 const Reviews = () => {
   const dispatch = useDispatch();
+  const [isLoading, setIsLoading] = useState(true);
 
   const reviews = useSelector((state) =>
     Object.values(state.review).sort((a, b) => b.id - a.id)
   );
+
+  useEffect(() => {
+    dispatch(fetchLatestReviews()).then(() => setIsLoading(false));
+  }, [dispatch]);
+
 }
 
 export default Reviews;
