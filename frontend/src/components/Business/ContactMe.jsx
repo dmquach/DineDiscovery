a76218fcf1b72b7a99377697d2300ac21e93b779
Dylan <dylanquach@gmail.com>
2024-04-18 13:57:29 -0700

    Add contact card

---
+++ frontend/src/components/Business/ContactMe.jsx
@@ -0,0 +1,22 @@
+const ContactMe=()=>{
+    return (
+      <div className="contactMeHolder">
+        <h1 className="contactMe">Contact Me</h1>
+
+        <div>
+          <h2>Full Stack Developer & Creative Designer</h2>
+          <p>Bridging the Gap Between Functionality & Aesthetics</p>
+        </div>
+
+
+        <a
+          className="resumeLink"
+          href="" target="_blank"
+        >
+          Resume
+        </a>
+      </div>
+    );
+}
+
+export default ContactMe
--- frontend/src/components/Homepage/reviews.jsx
+++ frontend/src/components/Homepage/reviews.jsx
@@ -2,12 +2,11 @@ import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchLatestReviews } from "../../store/review";
 
-import reviewCard from "./reviewCard";
+import ReviewCard from "./reviewCard";
 import "./reviewCard.css";
 
 const Reviews = () => {
   const dispatch = useDispatch();
-  const [isLoading, setIsLoading] = useState(true);
 
   const reviews = useSelector((state) =>
     Object.values(state.review).sort((a, b) => b.id - a.id)
