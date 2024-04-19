840af2a6e7ae08c9f65191865e371f07fd6405b8
Dylan <dylanquach@gmail.com>
2024-04-19 11:34:20 -0700

    Add fetch use effect

--- frontend/src/components/Post/post.jsx
+++ frontend/src/components/Post/post.jsx
@@ -2,4 +2,11 @@ function Post() {
   const [newPost, setNewPost] = useState(null);
   const [posts, setPosts] = useState([]);
 
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const res = await fetch("/api/posts");
+      setPosts(await res.json());
+    };
+    fetchPosts();
+  }, []);
 }
