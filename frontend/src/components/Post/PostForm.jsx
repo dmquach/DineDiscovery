333c47effe6ddabb2d20265d9bddc55023148cf1
Dylan <dylanquach@gmail.com>
2024-04-20 23:55:51 -0700

    Set target

--- frontend/src/components/Post/PostForm.jsx
+++ frontend/src/components/Post/PostForm.jsx
@@ -3,6 +3,10 @@ import { useState } from "react";
 function PostForm({setNewPost}) {
   const [title, setTitle] = useState("");
   const [photoFile, setPhotoFile] = useState(null);
+
+  const handleInput = (e) => {
+  setTitle(e.currentTarget.value);
+  };
 }
 
 export default PostForm;
