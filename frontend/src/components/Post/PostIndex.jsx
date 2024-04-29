0ca717c3f96a485e3a53166f1547f09876cbbb66
Dylan <dylanquach@gmail.com>
2024-04-28 23:58:33 -0700

    Add post index

--- frontend/src/components/Post/PostIndex.jsx
+++ frontend/src/components/Post/PostIndex.jsx
@@ -1,7 +1,16 @@
 function PostIndex({ posts }) {
   return (
    <ul>
-
+  {posts.map(post => {
+    return (
+      <li key={post.id}>
+        <h2>{post.title}</h2>
+        {post?.imageUrls?.map(imageUrl => (
+        ))}
+        {/* <img key={`${post.photoUrl}`} src={`${post.photoUrl}`} alt="" /> */}
+      </li>
+    );
+  })}
 </ul>
   );
 }
