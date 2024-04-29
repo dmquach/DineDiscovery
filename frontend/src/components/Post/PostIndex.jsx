10de2abe2908a87b1827cd4cea8e60cbab4ae9e8
Dylan <dylanquach@gmail.com>
2024-04-28 23:58:47 -0700

    Add image to index

--- frontend/src/components/Post/PostIndex.jsx
+++ frontend/src/components/Post/PostIndex.jsx
@@ -6,6 +6,7 @@ function PostIndex({ posts }) {
       <li key={post.id}>
         <h2>{post.title}</h2>
         {post?.imageUrls?.map(imageUrl => (
+          <img key={imageUrl} src={`${imageUrl}`} alt="" />
         ))}
         {/* <img key={`${post.photoUrl}`} src={`${post.photoUrl}`} alt="" /> */}
       </li>
