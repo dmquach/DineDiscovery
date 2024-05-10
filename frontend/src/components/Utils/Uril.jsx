2c0037dd0efd2d9bde84d05ea7b96fde00d6a2cf
Dylan <dylanquach@gmail.com>
2024-05-10 12:44:14 -0700

    Fix photo submit defaults

--- frontend/src/components/Utils/Uril.jsx
+++ frontend/src/components/Utils/Uril.jsx
@@ -1,3 +1,9 @@
 export const handleSinglePhotoSubmit = async (e) => {
+  e.preventDefault();
+  const formData = new FormData();
+  formData.append("post[title]", title);
+  if (photoFile) {
+    formData.append("post[photo]", photoFile);
+  }
 
 };
