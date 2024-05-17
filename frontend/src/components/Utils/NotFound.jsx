6c0c24a8f39ecb775ba89a93b792e2e6575cce05
Dylan <dylanquach@gmail.com>
2024-05-16 22:20:08 -0700

    Add not found

--- frontend/src/components/Utils/NotFound.jsx
+++ frontend/src/components/Utils/NotFound.jsx
@@ -2,7 +2,9 @@
 import { Link } from "react-router-dom/cjs/react-router-dom.min";
 const NotFound =()=>{
     return (
-
+        <div>
+          <h1>We’re sorry. We can’t find the page you’re looking for.</h1>
+        </div>
     );
 }
 
