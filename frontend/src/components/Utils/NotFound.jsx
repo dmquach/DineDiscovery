cabffed3a21747f0aaf387adf86b9cc021b13de5
Dylan <dylanquach@gmail.com>
2024-05-17 12:52:06 -0700

    Add css not found

--- frontend/src/components/Utils/NotFound.jsx
+++ frontend/src/components/Utils/NotFound.jsx
@@ -2,9 +2,15 @@ import "./NotFound.css"
 import { Link } from "react-router-dom/cjs/react-router-dom.min";
 const NotFound =()=>{
     return (
+      <div>
         <div>
           <h1>We’re sorry. We can’t find the page you’re looking for.</h1>
+          <h2>Please try a new </h2>
+          <Link to="/">
+            search
+          </Link>
         </div>
+      </div>
     );
 }
 
