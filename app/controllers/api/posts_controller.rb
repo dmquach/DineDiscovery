54fed5376d54f55acb615de436e79021e98ddd56
Dylan <dylanquach@gmail.com>
2024-06-10 18:54:05 -0700

    Add index controller post

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -1,3 +1,7 @@
 class Api::PostsController < ActionController::API
   wrap_parameters include: Post.attribute_names + [:images]
+
+  def index
+    @posts = Post.all.sort { |a, b| b.created_at <=> a.created_at }
+  end
 end
