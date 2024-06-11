529fdb912d422d7382c13230f9837573b1cca14c
Dylan <dylanquach@gmail.com>
2024-06-11 01:07:45 -0700

    Fix posts controller

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -4,4 +4,13 @@ class Api::PostsController < ActionController::API
   def index
     @posts = Post.all.sort { |a, b| b.created_at <=> a.created_at }
   end
+
+  def create
+    post = Post.new(post_params)
+    if post.save
+      render partial: "api/posts", locals: { post: post }
+    else
+      render json: post.errors, status: 422
+    end
+  end
 end
