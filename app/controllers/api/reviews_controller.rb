4861ed12e4305b6d06f01cf5c81057b41ff01d2d
Dylan <dylanquach@gmail.com>
2024-06-19 23:38:51 -0700

    Create review index controller

--- app/controllers/api/reviews_controller.rb
+++ app/controllers/api/reviews_controller.rb
@@ -2,4 +2,8 @@ class Api::ReviewsController < ApplicationController
   before_action :require_logged_in, only: [:create]
   wrap_parameters include: Review.attribute_names + [:images]
 
+  def index
+    @reviews = Review.includes(:user).all
+  end
+
 end
