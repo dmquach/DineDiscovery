0103618b1990a52963dbd775195dd8a740aec36c
Dylan <dylanquach@gmail.com>
2024-07-10 13:36:05 -0700

    Fix updated ratings

--- app/models/business.rb
+++ app/models/business.rb
@@ -7,6 +7,8 @@ class Business < ApplicationRecord
 
   def update_average_rating
     reviewers_count = reviewers.count
+    return update(rating: 0) if reviewers_count.zero?
+    new_average = reviews.sum(:rating) / reviewers_count.to_f
     update(rating: new_average)
   end
 
