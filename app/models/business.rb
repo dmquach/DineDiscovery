8d55d837fdcef6faf4ab5f1d4d5517b85068bbc4
Dylan <dylanquach@gmail.com>
2024-07-11 13:09:52 -0700

    Create business review connection

--- app/models/business.rb
+++ app/models/business.rb
@@ -5,6 +5,11 @@ class Business < ApplicationRecord
              class_name: :User,
              foreign_key: :user_id
 
+  has_many :reviews,
+             class_name: :Review,
+             foreign_key: :business_id,
+             dependent: :destroy
+
   def update_average_rating
     reviewers_count = reviewers.count
     return update(rating: 0) if reviewers_count.zero?
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,7 @@
 class Review < ApplicationRecord
   validates :body, presence: true
   validates :rating, presence: true
-  
+
+  belongs_to :business
+  belongs_to :user
 end
