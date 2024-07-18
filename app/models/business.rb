3174f6d32ebfe5aabd06c64e1b927fb71978ad84
Dylan <dylanquach@gmail.com>
2024-07-17 23:20:01 -0700

    Fix business validations

--- app/models/business.rb
+++ app/models/business.rb
@@ -2,6 +2,8 @@ class Business < ApplicationRecord
   validates :name, presence: true
   validates :city, presence: true
   validates :state, presence: true, length: { is: 2 }
+  validates :zip_code, presence: true, length: { is: 5 }
+  validates :phone_number, presence: true, length: { is: 10 }
   validates :price_range, presence: true
   validates :category, presence: true
 
