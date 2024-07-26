41f15bf50bfdf843ae312861bd92f9b5525b9005
Dylan <dylanquach@gmail.com>
2024-07-25 18:14:43 -0700

    Create default picture

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,8 @@ require "open-uri"
 
 class Post < ApplicationRecord
   validates :title, presence: true
-
+  validate :ensure_photo
+  before_validation :generate_default_pic
+  has_many_attached :images
+  has_one_attached :photo
 end
