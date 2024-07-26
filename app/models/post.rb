d853837a9b3371eb490dc3b4d1dadb62d94361b3
Dylan <dylanquach@gmail.com>
2024-07-25 18:15:11 -0700

    ensure default photo

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,4 +6,11 @@ class Post < ApplicationRecord
   before_validation :generate_default_pic
   has_many_attached :images
   has_one_attached :photo
+
+  def ensure_photo
+    unless self.photo.attached?
+      errors.add(:photo, "must be attached")
+    end
+  end
+
 end
