31918edfee7b0e7b30dac1ad4fa20dc4f239a827
Dylan <dylanquach@gmail.com>
2024-11-01 23:55:39 -0700

    Add active storage schema

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_02_220912) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "active_storage_attachments", force: :cascade do |t|
+
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
     t.string "first_name", null: false
