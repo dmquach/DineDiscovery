acc897115ced275ed5b12150aaa2ef5eb971f3fd
Dylan <dylanquach@gmail.com>
2024-11-09 22:57:07 -0800

    Fix active storage schema

--- db/schema.rb
+++ db/schema.rb
@@ -19,6 +19,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_02_220912) do
     t.string "record_type", null: false
     t.bigint "record_id", null: false
     t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
 
   create_table "users", force: :cascade do |t|
