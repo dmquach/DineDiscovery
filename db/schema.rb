28a2dad782cabcf2d0b8d2869b55b00714e7a610
Dylan <dylanquach@gmail.com>
2024-11-16 16:03:58 -0800

    Create active storage blob

--- db/schema.rb
+++ db/schema.rb
@@ -32,6 +32,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_02_220912) do
     t.string "service_name", null: false
     t.bigint "byte_size", null: false
     t.string "checksum"
+    t.datetime "created_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
