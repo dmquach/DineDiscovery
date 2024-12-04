4d2fdad1e1a0bb3a4312793a6cd528323d3ddc8e
Dylan <dylanquach@gmail.com>
2024-12-04 13:03:16 -0800

    Add star rating schema

--- db/schema.rb
+++ db/schema.rb
@@ -67,6 +67,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_02_220912) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "rating"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
