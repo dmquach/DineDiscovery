5769fa8a211117bc2f5350bfdb7004c4ba9793f1
Dylan <dylanquach@gmail.com>
2025-01-10 23:14:55 -0800

    Fix reviews schema

--- db/schema.rb
+++ db/schema.rb
@@ -89,6 +89,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_02_220912) do
     t.datetime "updated_at", null: false
     t.index ["business_id", "user_id"], name: "index_reviews_on_business_id_and_user_id", unique: true
     t.index ["business_id"], name: "index_reviews_on_business_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
 
